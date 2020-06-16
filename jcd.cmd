c74b1d43190a84f9b0679ea4b0c3f50f57e5927d
Jaime de los Hoyos M <jdeloshoyos@gmail.com>
2020-06-16 13:05:25 -0400

    1.09: Mejoras importantes en el Batch para procesar y capturar output del binario

--- jcd.cmd
+++ jcd.cmd
@@ -1,6 +1,24 @@
 @echo off
 chcp 1252
-for /f "tokens=*" %%i in ('jcd_bin.exe %1') do set OUTPUT=%%i
-echo JVA Directory Navigator
-echo Switching current directory to %OUTPUT% ...
-cd /d %OUTPUT%
+
+rem Parse the executable's output
+for /f "tokens=1,2,3 delims=*" %%A in ('jcd_bin.exe %1') do (
+set JCD_VERSION=%%A
+set JCD_COMMAND=%%B
+set JCD_ARGUMENT=%%C
+)
+
+rem Take action based on executable's output
+if %JCD_COMMAND%==ERROR (
+echo JVA Directory Navigator %JCD_VERSION%
+echo Error: %JCD_ARGUMENT%
+) ELSE (
+echo JVA Directory Navigator %JCD_VERSION%
+echo Switching current directory to %JCD_ARGUMENT% ...
+cd /d %JCD_ARGUMENT%
+)
+
+rem Clean up environment variables
+set JCD_VERSION=
+set JCD_COMMAND=
+set JCD_ARGUMENT=
